e4dc6682e6ae5a23d34d5120c990fc6286811db7
hppavilion1 <nathantronnes@gmail.com>
2015-01-11 13:26:05 -0900

    KnowPapa Browser
    Added the VERY simple KnowPapa Browser for now. It'll be just the
    general browser while I start out.

---
+++ knowpapa browser/gui.py
@@ -0,0 +1,22 @@
+#!usr/bin/python
+from Tkinter import *
+import urlhandler
+def go():
+	text.delete(1.0, END)
+	conn = urlhandler.urlhandler(entry.get())
+	conn.openURL()
+	text.insert(INSERT, conn.getURLdata())
+	conn.closeURL()
+browserwindow = Tk()
+browserwindow.title('knowpapa browser')
+label = Label(browserwindow, text= 'url:')
+entry = Entry(browserwindow)
+button = Button(browserwindow, text='Go', command = go)
+text = Text(browserwindow)
+label.pack(side=TOP)
+entry.pack(side=TOP)
+button.pack(side=TOP)
+text.pack(side= TOP)
+browserwindow.mainloop()
+
+ 
